5080516dfc89a3347316ddaf3a8a8db7c6a4c4f3
Henry <ezeosy@gmail.com>
2016-02-10 19:31:09 +0000

    First push on this project

---
+++ src/test/java/CDPlayerTest.java
@@ -0,0 +1,26 @@
+import configuration.CDPlayerConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import soundsystem.CompactDisc;
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes= CDPlayerConfig.class)
+public class CDPlayerTest {
+
+
+    @Autowired
+    private CompactDisc cd;
+
+    @Test
+    public void cdShouldNotBeNull(){
+        assertNotNull(cd);
+    }
+
+
+
+
+}
